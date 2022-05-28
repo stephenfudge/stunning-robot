a43e9425c88dbf37dcf73ef44de27ab7ebc527dc
Stephen Fudge <sfudge@gmail.com>
2022-05-28 19:30:15 -0400

    added html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
+// user inputs city in form - make variable
+// fetch current/future weather for that city put into local storage
+// for weather need city name, the date, an icon representation of weather conditions,
+//  the temperature, the humidity, the wind speed, and the UV index (color coded favorable(green) moderate(yellow) severe(red))
+// 
+
+
 var APIkey = "0d79bd3ff7e85c4b19f6f08702035eed";
 
 var city;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
 
-fetch(queryURL)
