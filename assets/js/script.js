09b6d94ea7d4a08109b5234070ed68a0e95417d8
Stephen Fudge <sfudge@gmail.com>
2022-05-29 14:32:57 -0400

    created variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,23 @@
 // 
 
 
+var cityInput = document.getElementById("cityInput");
+var cityForm = document.getElementById("cityForm");
+var submitButton = document.getElementById("submitButton");
+var searchHistory = document.getElementById("history")
+var currentWeather = document.getElementById("currentWeather")
+var fiveDayForecast = document.getElementById("fiveDay")
+
+
+// key for openweatherapi 
 var APIkey = "0d79bd3ff7e85c4b19f6f08702035eed";
 
-var city;
+// do i want to do 'city' or 'cities'? 
+var city; //var cities = []
+
+
+
+//submit button
+submitButton.on("submit", function () {
+
+})
